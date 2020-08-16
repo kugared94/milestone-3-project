c380a3e76ef8fa8eef6f71e6c2fa50800144610d
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-08-16 14:34:03 +0000

    Add Session User Profile

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ def register():
 
             session["user"] = request.form.get("username").lower()
             flash("Registration Successful")
+            return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -63,6 +65,8 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
@@ -70,8 +74,15 @@ def login():
         else:
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-            
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 
