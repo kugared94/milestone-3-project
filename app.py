9189716bb766abe4bb393ded45e774f38d3de1aa
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-08-16 12:52:41 +0000

    Add Register funtinaltiy

--- app.py
+++ app.py
@@ -27,6 +27,21 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+            existing_user = mongo.db.users.find_one(
+                {"username": request.form.get("username").lower()})
+            if existing_user:
+                flash("Username Already Exists")
+                return redirect(url_for("register"))
+
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+
+            session["user"] = request.form.get("username").lower()
+            flash("Registration Successful")
     return render_template("register.html")
 
 
