590994af9719a4d2cebcf493baa9a7b5cda5d369
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-08-17 08:07:44 +0000

    Bind data to the edit task form

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
-    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    task = mongo.db.recipe.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_recipe.html", task=task, categories=categories)
 
